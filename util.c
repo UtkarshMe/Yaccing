17e6c13e42b94ed12f0c20575e6fa25eaa0b4277
UTkarsh Maheshwari <utkarshme96@gmail.com>
2018-03-17 23:29:23 +0530

    Display fields in table with NULL for missing val

--- util.c
+++ util.c
@@ -3,14 +3,43 @@
 #include <string.h>
 #include "types.h"
 
+char *fields[] = {
+    "address",
+    "author",
+    "booktitle",
+    "chapter",
+    "edition",
+    "journal",
+    "number",
+    "pages",
+    "publisher",
+    "school",
+    "title",
+    "volume",
+    "year",
+    NULL
+};
+
 void entry_show(Entry entry)
 {
+    int size = entry.size;
+    char *value = NULL;
+    char *null = "NULL";
+
     printf("\n");
-    printf("Type\t: %s\n", entry.name);
-    printf("Key\t: %s\n", entry.key);
-    printf("Fields\t: \n");
-    for (int i = 0; i < entry.size; ++i) {
-        printf("\t%s\t: %s\n", entry.fields[i].name, entry.fields[i].value);
+    printf("bibtype\t: %s\n", entry.name);
+    printf("bibkey\t: %s\n", entry.key);
+
+    for (int i = 0; fields[i] != NULL; ++i) {
+        for (int j = 0; j < size; ++j) {
+            if (strcmp(fields[i], entry.fields[j].name) == 0) {
+                value = entry.fields[j].value;
+                break;
+            } else {
+                value = null;
+            }
+        }
+        printf("%s\t: %s\n", fields[i], value);
     }
     printf("\n");
 }
